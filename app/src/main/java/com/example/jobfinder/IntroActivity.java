f880f5a679815bce87a980768c099ccf4fe2d6ed
Junimo05 <101935287+Junimo05@users.noreply.github.com>
2024-05-26 13:34:38 +0700

    first

---
+++ app/src/main/java/com/example/jobfinder/IntroActivity.java
@@ -0,0 +1,8 @@
+package com.example.jobfinder;
+
+public class IntroActivity {
+    // This is a placeholder for the IntroActivity class
+    // This class will be implemented in the next step
+
+
+}
